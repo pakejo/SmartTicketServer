0aaf6e399a7d3ddae8bb7ea6fd627c26dd518e82
Francisco Ruiz <paco1998@hotmail.es>
2022-07-24 16:07:44 +0200

    Added custom event endpoint

--- smarticket_api/views.py
+++ smarticket_api/views.py
@@ -1,4 +1,7 @@
+import datetime
+
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from smarticket_api.serializers import *
@@ -16,6 +19,12 @@ class EventsViewSets(viewsets.ModelViewSet):
             queryset = self.queryset
 
         serializer = EventSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False)
+    def future_events(self, request):
+        future_events = self.queryset.filter(date__gt=datetime.datetime.now())
+        serializer = EventSerializer(future_events, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
