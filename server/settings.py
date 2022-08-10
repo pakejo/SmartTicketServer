14256f1b2b194911240ffe811e7d48f55864736a
Francisco Ruiz <paco1998@hotmail.es>
2022-08-10 20:24:35 +0200

    Updated static files paths

--- server/settings.py
+++ server/settings.py
@@ -141,12 +141,12 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # For deploy in heroku
-MEDIA_URL = '/uploads/'
+MEDIA_URL = '/static/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
